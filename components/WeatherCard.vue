6c614581a0d96804d54f70f1e96b97d0b8c5d4c2
thomas.convain <thomas.convain@gmail.com>
2024-01-12 22:42:32 -0300

    redondea las temperaturas

--- components/WeatherCard.vue
+++ components/WeatherCard.vue
@@ -7,7 +7,7 @@
     <div class="px-4 py-5 sm:p-6">
       <div class="grid grid-cols-2 flex items-center">
         <img :src="getWeatherIconUrl(weatherData.weather[0].icon,'@4x')" alt="Weather Icon" />
-        <p>{{ weatherData.main.temp }}°C</p>
+        <p>{{ Math.round(weatherData.main.temp) }}°C</p>
       </div>
     </div>
     <div class="px-4 py-4 sm:px-6">
@@ -15,9 +15,9 @@
       <!-- We use less vertical padding on card footers at all sizes than on headers or body sections -->
       <div v-for="item in weatherForecast" :key="item.dt" class="my-4 grid grid-cols-2 flex items-center">
         {{ item.dayOfWeek }}
-        <div>
+        <div class="flex items-center">
           <div><img :src="getWeatherIconUrl(item.weather[0].icon, '')"/></div>
-          <div>{{ item.main.temp }}ºC</div>
+          <div>{{ Math.round(item.main.temp) }}ºC</div>
         </div>
     </div>
     </div>
