64639d186fc612abb87d32b4a96a1e0ec4269cf2
thomas.convain <thomas.convain@gmail.com>
2024-01-12 23:15:51 -0300

    mejora el ui

--- components/WeatherCard.vue
+++ components/WeatherCard.vue
@@ -1,20 +1,25 @@
 <template>
   <div>
-    <div class="bg-white overflow-hidden shadow rounded-lg divide-y divide-gray-200">
+    <div class="bg-white overflow-hidden rounded-2xl divide-y divide-gray-100">
     <div class="px-4 py-5 sm:px-6">
-      <h2 class="font-semibold">{{ weatherData.name }}, {{ weatherData.sys.country }}</h2>
+      <span class="inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-neutral-100 text-neutral-800">
+        <svg class="-ml-0.5 mr-1.5 h-2 w-2 text-neutral-400" fill="currentColor" viewBox="0 0 8 8">
+          <circle cx="4" cy="4" r="3" />
+        </svg>
+        <h2 class="font-semibold">{{ weatherData.name }}, {{ weatherData.sys.country }}</h2>
+    </span>
     </div>
     <div class="px-4 py-5 sm:p-6">
       <div class="grid grid-cols-2 flex items-center">
         <img :src="getWeatherIconUrl(weatherData.weather[0].icon,'@4x')" alt="Weather Icon" />
-        <p>{{ Math.round(weatherData.main.temp) }}°C</p>
+        <p class="text-3xl">{{ Math.round(weatherData.main.temp) }}°C</p>
       </div>
     </div>
-    <div class="px-4 py-4 sm:px-6">
+    <div class="px-4 py-4 sm:px-6 divide-y divide-gray-50 ">
       <!-- Content goes here -->
       <!-- We use less vertical padding on card footers at all sizes than on headers or body sections -->
-      <div v-for="item in weatherForecast" :key="item.dt" class="my-4 grid grid-cols-2 flex items-center">
-        {{ item.dayOfWeek }}
+      <div v-for="item in weatherForecast" :key="item.dt" class="my-4 grid grid-cols-2 flex items-center ">
+        <div>{{ item.dayOfWeek }}</div>
         <div class="flex items-center">
           <div><img :src="getWeatherIconUrl(item.weather[0].icon, '')"/></div>
           <div>{{ Math.round(item.main.temp) }}ºC</div>
@@ -41,7 +46,7 @@ export default {
     getWeatherIconUrl(iconCode, size) {
       // Reemplaza '10d' con el código de icono proporcionado por la API
       // Puedes adaptar este código según tus necesidades
-      return `http://openweathermap.org/img/wn/${iconCode}${size}.png`;
+      return `https://openweathermap.org/img/wn/${iconCode}${size}.png`;
     },
   },
 };
